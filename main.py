0ecb48bd542ebf534752e6b7e94b9c0ee5fa22a5
bobby <bogoblin@gmail.com>
2023-10-11 14:26:23 +0100

    Set album artist and date

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import tempfile
+
 from yt_dlp import YoutubeDL
 from ytmusicapi import YTMusic
 import pathlib
@@ -8,37 +10,44 @@ import os
 from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
 
+
 def download_album(ytmusic, query):
     search = ytmusic.search(query, 'albums')
     first_result = search[0]
     browse_id = first_result['browseId']
     album = ytmusic.get_album(browse_id)
     playlist_id = album['audioPlaylistId']
-    artist = ', '.join([artist['name'] for artist in album['artists']])
+    album_artist = ', '.join([artist['name'] for artist in album['artists']])
     album_name = album['title']
 
-    # Create output directory:
-    music_dir = pathlib.Path('D:\\Music')
-    artist_dir = music_dir / artist
-    if not artist_dir.exists():
-        os.mkdir(artist_dir)
-    album_dir = artist_dir / album_name
-    if not album_dir.exists():
-        os.mkdir(album_dir)
+    # We create a temporary directory to work in, otherwise
+    # foobar2000 can start reading the files:
+    temp_dir = tempfile.mkdtemp()
 
     # Download album cover:
     # Thumbnails are sorted, with highest resolution last, so pick that one:
     thumbnail_url = album['thumbnails'][-1]['url']
     thumbnail_response = requests.get(thumbnail_url, stream=True)
     if thumbnail_response.status_code == 200:
-        thumbnail_path = pathlib.Path(album_dir, 'cover.jpg')
+        thumbnail_path = pathlib.Path(temp_dir, 'cover.jpg')
         with open(thumbnail_path, 'wb') as f:
             shutil.copyfileobj(thumbnail_response.raw, f)
 
+    # Create output directory:
+    music_dir = pathlib.Path('D:\\Music')
+    artist_dir = music_dir / album_artist
+    if not artist_dir.exists():
+        os.mkdir(artist_dir)
+    album_dir = artist_dir / album_name
+    if not album_dir.exists():
+        os.mkdir(album_dir)
+
+
     with YoutubeDL({
         'paths': {
-            'home': str(album_dir)
+            'home': str(temp_dir)
         },
+        'format': 'm4a/bestaudio/best',
         'postprocessors': [
             { 'key': 'FFmpegMetadata', },
             { 'key': 'FFmpegThumbnailsConvertor', },
@@ -57,16 +66,13 @@ def download_album(ytmusic, query):
                 try:
                     mp3 = MP3(file_path, ID3=EasyID3)
                     mp3['tracknumber'] = f'{track_number+1}/{total_tracks}'
+                    mp3['albumartist'] = album_artist
+                    mp3['date'] = f'{album["year"]}'
                     mp3.save()
+                    shutil.move(file_path, album_dir)
                     break
                 except:
                     pass
-
-
-
-    # zip_path = pathlib.Path(shutil.make_archive(album_directory, 'zip', album_directory))
-    # shutil.move(zip_path, pathlib.Path("D:\\Music", zip_path.name))
-    # shutil.rmtree(album_directory)
 
 
 if __name__ == '__main__':
