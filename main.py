84e58dfee32bb83229ae63b8c15cfbc2a115b966
bobby <bogoblin@gmail.com>
2023-10-11 15:18:49 +0100

    Some tidy up

--- main.py
+++ main.py
@@ -11,12 +11,8 @@ from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
 
 
-def download_album(ytmusic, query):
-    search = ytmusic.search(query, 'albums')
-    first_result = search[0]
-    browse_id = first_result['browseId']
+def download_album(browse_id, ytmusic=YTMusic()):
     album = ytmusic.get_album(browse_id)
-    playlist_id = album['audioPlaylistId']
     album_artist = ', '.join([artist['name'] for artist in album['artists']])
     album_name = album['title']
 
@@ -26,22 +22,15 @@ def download_album(ytmusic, query):
 
     # Create output directory:
     music_dir = pathlib.Path('D:\\Music')
-    artist_dir = music_dir / album_artist
-    if not artist_dir.exists():
-        os.mkdir(artist_dir)
-    album_dir = artist_dir / album_name
-    if not album_dir.exists():
-        os.mkdir(album_dir)
+    album_dir = music_dir / album_artist / album_name
+    os.makedirs(album_dir, True)
 
     # Download album cover:
     # Thumbnails are sorted, with highest resolution last, so pick that one:
-    thumbnail_url = album['thumbnails'][-1]['url']
-    thumbnail_response = requests.get(thumbnail_url, stream=True)
+    thumbnail_response = requests.get(album['thumbnails'][-1]['url'], stream=True)
     if thumbnail_response.status_code == 200:
-        thumbnail_path = pathlib.Path(album_dir, 'cover.jpg')
-        with open(thumbnail_path, 'wb') as f:
+        with open(album_dir / 'cover.jpg', 'wb') as f:
             shutil.copyfileobj(thumbnail_response.raw, f)
-
 
     with YoutubeDL({
         'paths': {
@@ -49,8 +38,8 @@ def download_album(ytmusic, query):
         },
         'format': 'm4a/bestaudio/best',
         'postprocessors': [
-            { 'key': 'FFmpegMetadata', },
-            { 'key': 'FFmpegThumbnailsConvertor', },
+            {'key': 'FFmpegMetadata', },
+            {'key': 'FFmpegThumbnailsConvertor', },
             {  # Extract audio using ffmpeg
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -58,14 +47,14 @@ def download_album(ytmusic, query):
         ],
         # 'keepvideo': True
     }) as ydl:
-        info = ydl.extract_info(playlist_id)
+        info = ydl.extract_info(album['audioPlaylistId'])
         total_tracks = len(info['entries'])
         for track_number, entry in enumerate(info['entries']):
             for download in entry['requested_downloads']:
                 file_path = download['filepath']
                 try:
                     mp3 = MP3(file_path, ID3=EasyID3)
-                    mp3['tracknumber'] = f'{track_number+1}/{total_tracks}'
+                    mp3['tracknumber'] = f'{track_number + 1}/{total_tracks}'
                     mp3['albumartist'] = album_artist
                     mp3['date'] = f'{album["year"]}'
                     mp3.save()
@@ -74,7 +63,11 @@ def download_album(ytmusic, query):
                 except:
                     pass
 
+    print(f'Downloaded album to {album_dir}')
+
 
 if __name__ == '__main__':
-    yt_music = YTMusic()
-    download_album(yt_music, ' '.join(sys.argv[1:]))
+    query = ' '.join(sys.argv[1:])
+    ytmusic = YTMusic()
+    search = ytmusic.search(query, 'albums')
+    download_album(search[0]['browseId'], ytmusic=ytmusic)
