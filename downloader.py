226bfd3cf1d26b0753e0558f8c81321ae2c4413d
bobby <bogoblin@gmail.com>
2023-10-12 18:26:38 +0100

    Nicer TUI

--- downloader.py
+++ downloader.py
@@ -21,7 +21,7 @@ def download_album(browse_id, ytmusic=YTMusic()):
     # Create output directory:
     music_dir = pathlib.Path('D:\\Music')
     album_dir = music_dir / album_artist / album_name
-    os.makedirs(album_dir, True)
+    os.makedirs(album_dir, 0o777, True)
 
     # Download album cover:
     # Thumbnails are sorted, with highest resolution last, so pick that one:
@@ -54,6 +54,7 @@ def download_album(browse_id, ytmusic=YTMusic()):
                     mp3 = MP3(file_path, ID3=EasyID3)
                     mp3['tracknumber'] = f'{track_number + 1}/{total_tracks}'
                     mp3['albumartist'] = album_artist
+                    mp3['artist'] = album_artist
                     mp3['date'] = f'{album["year"]}'
                     mp3.save()
                     shutil.move(file_path, album_dir)
--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from ytmusicapi import YTMusic
 from downloader import download_album
 
 if __name__ == '__main__':
-    query = ' '.join(sys.argv[1:])
     ytmusic = YTMusic()
-    search = ytmusic.search(query, 'albums')
-    download_album(search[0]['browseId'], ytmusic=ytmusic)
+    while True:
+        query = input("Enter album name and artist: ")
+        if len(query) == 0:
+            exit(0)
+        search = ytmusic.search(query, 'albums')
+        for i, album in enumerate(search):
+            album_artist = ', '.join([artist['name'] for artist in album['artists']])
+            print(f'{i+1}. {album_artist} - {album["title"]}')
+        choice = int(input("Enter choice: "))
+        album = search[choice - 1]
+        download_album(album['browseId'], ytmusic=ytmusic)
